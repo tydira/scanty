3717036d80b45802360b05dc395a79aadea62f42
Justin Krueger <justin@kroo.gs>
2017-08-25 01:01:20 -0700

    Remove an unnecessary isFunction() call.

--- src/lexer.js
+++ src/lexer.js
@@ -65,7 +65,7 @@ export default class Lexer extends Emitter {
 
         const context = { rule, token, tokens, found }
 
-        if (isFunction(rule.handler)) context.action = rule.handler
+        if (rule.handler) context.action = rule.handler
         this.emit(rule.name, context)
         if (isFunction(context.action)) context.action(context)
 
